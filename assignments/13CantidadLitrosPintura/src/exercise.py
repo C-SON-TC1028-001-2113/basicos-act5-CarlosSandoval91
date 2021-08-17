be8b607d5a06eb96728b5a62884a303c724340a5
CarlosSandoval91 <A01252494@itesm.mx>
2021-08-17 17:42:27 +0000

    AREADE PINTURA

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,7 +1,13 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+    A=float(input('Area a pintar en metros: '))
+    R=float(input('Rendimiento (m2/l): '))
+    L=int(math.ceil(A/R))
+    print('Litros a comprar: '+str(L))
 
 if __name__ == '__main__':
     main()
+#Area a pintar en metros: 857.5
+#Rendimiento (m2/l): 10
+#Litros a comprar: 86
